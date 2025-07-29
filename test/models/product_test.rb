21fcd8265c0569e34b1bbad59b927603787fd535
Heather Knowlton <heather.knowlton@shopify.com>
2025-07-29 15:36:03 -0400

    styling and testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # First, we include the Action Mailer test helpers so we can monitor emails sent during the test.
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back in stock" do
+    # The tshirt fixture is loaded using the products() fixture helper and returns the Active Record object for that record.
+    product = products(:tshirt)
+
+    # Set product out of stock
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
